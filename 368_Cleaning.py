9983ea11c0bcfaef9c5e215341a1487ee1c6d4dd
dennisjob <55144483+dennisjob@users.noreply.github.com>
2020-11-01 14:01:37 -0500

    Add files via upload
    very cool cleaning complete

---
+++ 368_Cleaning.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+
+def calculate_seconds(minsec):
+  if isinstance(minsec, float):
+    return np.nan 
+  else:
+    m, s = minsec.split(':')
+    return int(m) * 60 + int(s)
+  
+def split_column_first(string_to_split):
+  first, second = string_to_split.split(' of ')
+  return first
+
+def split_column_second(string_to_split):
+  first, second = string_to_split.split(' of ')
+  return second
+
+df_raw = pd.read_csv(r'DataStorage\\fight_stats.csv')
+df = df_raw.drop('Unnamed: 0',axis=1)
+
+#Replace values
+mapping_dict = {"L": 0, "W": 1, '---': np.nan, '--': np.nan, "25:00:00": "25:00", "24:56:00": "24:56", "24:59:00": "24:59", "24:10:00": "24:10"}
+df.replace(mapping_dict, inplace=True)
+
+# Convert time into integer seconds
+df['TIME'] = df['TIME'].apply(calculate_seconds)
+df['M_CTRL'] = df['M_CTRL'].apply(calculate_seconds)
+df['OP_CTRL'] = df['OP_CTRL'].apply(calculate_seconds)
+
+# Split 'X of Y' columns into separate 'X' and 'Y' columns
+columns_to_split = ['M_SIG_STR', 'M_TOTAL_STR', 'M_TD', 'M_HEAD', 'M_BODY', 'M_LEG', 'M_DIST', 'M_CLIN', 'M_GROUND', 'OP_SIG_STR', 'OP_TOTAL_STR' ,'OP_TD', 'OP_HEAD', 'OP_BODY', 'OP_LEG', 'OP_DIST', 'OP_CLIN', 'OP_GROUND']
+columns_to_remove = ['M_SIG_STR', 'M_TOTAL_STR', 'M_TD', 'M_HEAD', 'M_BODY', 'M_LEG', 'M_DIST', 'M_CLIN', 'M_GROUND', 'OP_SIG_STR', 'OP_TOTAL_STR' ,'OP_TD', 'OP_HEAD', 'OP_BODY', 'OP_LEG', 'OP_DIST', 'OP_CLIN', 'OP_GROUND']
+
+for i in columns_to_split:
+  first_column = df[i].apply(split_column_first)
+  second_column = df[i].apply(split_column_second)
+  df[i + "_SUCC"] = pd.to_numeric(first_column)
+  df[i + "_TOTAL"] = pd.to_numeric(second_column)
+
+df.drop(columns_to_remove,inplace=True,axis=1)
+
+df.to_csv(r'DataStorage\\cleaned_fight_stats.csv')
