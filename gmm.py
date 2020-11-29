853b8878ec4913ce7f3c88a90f836d3cf99512bf
William Jereza <williamjereza@Williams-MacBook-Air.local>
2020-11-29 13:53:16 -0500

    take 2

---
+++ gmm.py
@@ -0,0 +1,58 @@
+import warnings 
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.metrics import silhouette_score
+from google.colab import drive
+drive.mount('drive')
+
+#read data and get columns
+df = pd.read_csv('/content/clustering_stats.csv')
+df.head()
+
+#getting the columns for the model 
+columns_df = df.iloc[:, 1:]
+columns_df = columns_df.fillna(0)
+features = list(columns_df.columns.values)
+
+df = pd.read_csv(r'DataStorage/fighter_details_links.csv')
+col_df = df.iloc[:, 1:]
+col_df = col_df.fillna(0)
+features = list(cl_df.columns.values)
+
+#z-score the data (not needed)
+#X = df[features]
+#z = StandardScaler()
+#X[features] = z.fit_transform(X)
+
+#Concluded on five clusters: fit the model and estimate paramaters
+X = df[features]
+GM = GaussianMixture(max_iter=100,n_components = 5,n_init=3)
+GM.fit(X)
+
+#silhoutte score
+print ("SILHOUTTE: ", silhouette_score(X, cluster))
+
+cluster = GM.predict(X)
+cluster_p = GM.predict_proba(X)
+#iterate to create rows and columns to append
+rows = []
+cluster_vector = cluster_p.tolist()
+for i in range (0,len(cluster)):
+    row = cluster_vector[i].copy()
+    row.insert(0,fighters_list[i])
+    rows.append(row)
+cols = ['Name','p(cluster_1)','p(cluster_2)','p(cluster_3)','p(cluster_4)','p(cluster_5)']
+
+#add to csv file
+table = pd.DataFrame(rows, columns=cols)
+table.to_csv(r'gmm.csv', index=False)  
+!cp gmm.csv "drive/My Drive/"
